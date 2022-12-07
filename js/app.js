d201d65ef7af187a37c960a50514e17161753759
Tal <trimoni8@gmail.com>
2022-12-07 09:56:21 -0500

    pokemon types

--- js/app.js
+++ js/app.js
@@ -22,5 +22,5 @@ async function getPokemon(num) {
   // console.log(pokemonDesc)
   pokemonDesc = pokemonDesc['flavor_text_entries'][10]['flavor_text']
 
-  pokedex[num] = {'name' : pokemonName, 'img' : pokemonImg}
+  pokedex[num] = {'name' : pokemonName, 'img' : pokemonImg, 'types' : pokemonTypes, }
 }
