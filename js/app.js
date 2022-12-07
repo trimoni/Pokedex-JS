43feea25dd379d07b869fe8a5e3a09ec86866569
Tal <trimoni8@gmail.com>
2022-12-07 09:36:11 -0500

    stub function getPokemon

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ const pokemonCount = 151
 let pokedex = {} // {1 : {'name' : bulbasaur, img : url, type : [grass, poison], desc : "..."}}
 
 window.onload = function() {
+  getPokemon(1)
+}
+
+function getPokemon(num) {
   
 }
