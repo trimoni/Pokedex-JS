15465252f86bce249064b5e38978fa9b366bb8be
Tal <trimoni8@gmail.com>
2022-12-07 10:16:50 -0500

    typesDiv append

--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ function updatePokemon(){
     let type = document.createElement('span')
     type.innerText = types[i]['type']['name'].toUpperCase()
     type.classList.add('type-box')
-    type.classList.add(types[i]['type']['name'])
+    type.classList.add(types[i]['type']['name']) // adds background color and font color
+    typesDiv.append(type)
   }
 }
